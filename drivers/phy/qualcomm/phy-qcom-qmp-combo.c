91d6231f44db199b179b04054b6bcccce76b8bac
Robin Reckmann <robin.reckmann@gmail.com>
2023-02-04 19:24:29 +0900

    Revert "phy: qcom-qmp: Register as a typec switch for orientation detection"
    This reverts commit c7db9d540bf580f967d27d80a7b7a80c8241eeb6.

--- drivers/phy/qualcomm/phy-qcom-qmp-combo.c
+++ drivers/phy/qualcomm/phy-qcom-qmp-combo.c
@@ -19,7 +19,6 @@
 #include <linux/regulator/consumer.h>
 #include <linux/reset.h>
 #include <linux/slab.h>
-#include <linux/usb/typec_mux.h>
 
 #include <dt-bindings/phy/phy-qcom-qmp.h>
 
@@ -62,10 +61,6 @@
 
 /* QPHY_V3_PCS_MISC_CLAMP_ENABLE register bits */
 #define CLAMP_EN				BIT(0) /* enables i/o clamp_n */
-
-/* QPHY_V3_DP_COM_TYPEC_CTRL register bits */
-#define SW_PORTSELECT_VAL			BIT(0)
-#define SW_PORTSELECT_MUX			BIT(1)
 
 #define PHY_INIT_COMPLETE_TIMEOUT		10000
 
@@ -909,9 +904,6 @@ struct qmp_combo {
 	struct mutex phy_mutex;
 	int init_count;
 
-	struct typec_switch_dev *sw;
-	enum typec_orientation orientation;
-
 	struct phy *usb_phy;
 	enum phy_mode mode;
 
@@ -1393,25 +1385,30 @@ static void qmp_v3_configure_dp_tx(struct qmp_combo *qmp)
 
 static bool qmp_combo_configure_dp_mode(struct qmp_combo *qmp)
 {
-	const struct phy_configure_opts_dp *dp_opts = &qmp->dp_opts;
 	u32 val;
-	bool reverse = qmp->orientation == TYPEC_ORIENTATION_REVERSE;
+	bool reverse = false;
 
 	val = DP_PHY_PD_CTL_PWRDN | DP_PHY_PD_CTL_AUX_PWRDN |
 	      DP_PHY_PD_CTL_PLL_PWRDN | DP_PHY_PD_CTL_DP_CLAMP_EN;
 
-	if (dp_opts->lanes == 4 || reverse)
-		val |= DP_PHY_PD_CTL_LANE_0_1_PWRDN;
-	if (dp_opts->lanes == 4 || !reverse)
-		val |= DP_PHY_PD_CTL_LANE_2_3_PWRDN;
-
+	/*
+	 * TODO: Assume orientation is CC1 for now and two lanes, need to
+	 * use type-c connector to understand orientation and lanes.
+	 *
+	 * Otherwise val changes to be like below if this code understood
+	 * the orientation of the type-c cable.
+	 *
+	 * if (lane_cnt == 4 || orientation == ORIENTATION_CC2)
+	 *	val |= DP_PHY_PD_CTL_LANE_0_1_PWRDN;
+	 * if (lane_cnt == 4 || orientation == ORIENTATION_CC1)
+	 *	val |= DP_PHY_PD_CTL_LANE_2_3_PWRDN;
+	 * if (orientation == ORIENTATION_CC2)
+	 *	writel(0x4c, qmp->dp_dp_phy + QSERDES_V3_DP_PHY_MODE);
+	 */
 	val |= DP_PHY_PD_CTL_LANE_2_3_PWRDN;
 	writel(val, qmp->dp_dp_phy + QSERDES_DP_PHY_PD_CTL);
 
-	if (reverse)
-		writel(0x4c, qmp->pcs + QSERDES_DP_PHY_MODE);
-	else
-		writel(0x5c, qmp->pcs + QSERDES_DP_PHY_MODE);
+	writel(0x5c, qmp->dp_dp_phy + QSERDES_DP_PHY_MODE);
 
 	return reverse;
 }
@@ -1802,7 +1799,6 @@ static int qmp_combo_com_init(struct qmp_combo *qmp)
 {
 	const struct qmp_phy_cfg *cfg = qmp->cfg;
 	void __iomem *com = qmp->com;
-	u32 val;
 	int ret;
 
 	mutex_lock(&qmp->phy_mutex);
@@ -1840,12 +1836,8 @@ static int qmp_combo_com_init(struct qmp_combo *qmp)
 			SW_DPPHY_RESET_MUX | SW_DPPHY_RESET |
 			SW_USB3PHY_RESET_MUX | SW_USB3PHY_RESET);
 
-	/* Latch CC orientation based on reported state by TCPM */
-	val = SW_PORTSELECT_MUX;
-	if (qmp->orientation == TYPEC_ORIENTATION_REVERSE)
-		val |= SW_PORTSELECT_VAL;
-
-	qphy_setbits(com, QPHY_V3_DP_COM_TYPEC_CTRL, val);
+	/* Default type-c orientation, i.e CC1 */
+	qphy_setbits(com, QPHY_V3_DP_COM_TYPEC_CTRL, 0x02);
 
 	qphy_setbits(com, QPHY_V3_DP_COM_PHY_MODE_CTRL, USB3_MODE | DP_MODE);
 
@@ -2181,52 +2173,6 @@ static const struct dev_pm_ops qmp_combo_pm_ops = {
 	SET_RUNTIME_PM_OPS(qmp_combo_runtime_suspend,
 			   qmp_combo_runtime_resume, NULL)
 };
-
-#if IS_ENABLED(CONFIG_PHY_QCOM_QMP_TYPEC)
-static int qcom_qmp_phy_typec_switch_set(struct typec_switch_dev *sw,
-					 enum typec_orientation orientation)
-{
-	struct qmp_combo *qmp = typec_switch_get_drvdata(sw);
-
-	dev_dbg(qmp->dev, "Toggling orientation current %d requested %d\n",
-		qmp->orientation, orientation);
-
-	qmp->orientation = orientation;
-
-	if (orientation == TYPEC_ORIENTATION_NONE) {
-		if (qmp->init_count)
-			qmp_combo_usb_power_off(qmp->usb_phy);
-	} else {
-		if (!qmp->init_count)
-			qmp_combo_usb_power_on(qmp->usb_phy);
-	}
-
-	return 0;
-}
-
-static int qcom_qmp_phy_typec_switch_register(struct qmp_combo *qmp)
-{
-	struct typec_switch_desc sw_desc;
-	struct device *dev = qmp->dev;
-
-	sw_desc.drvdata = qmp;
-	sw_desc.fwnode = dev->fwnode;
-	sw_desc.set = qcom_qmp_phy_typec_switch_set;
-	qmp->sw = typec_switch_register(dev, &sw_desc);
-	if (IS_ERR(qmp->sw)) {
-		dev_err(dev, "Error registering typec switch: %ld\n",
-			PTR_ERR(qmp->sw));
-	}
-
-	return 0;
-}
-#else
-static int qcom_qmp_phy_typec_switch_register(struct qmp_combo *qmp)
-{
-	return 0;
-}
-#endif
-
 
 static int qmp_combo_vreg_init(struct qmp_combo *qmp)
 {
@@ -2809,8 +2755,6 @@ static int qmp_combo_probe(struct platform_device *pdev)
 	phy_set_drvdata(qmp->dp_phy, qmp);
 
 	dev_set_drvdata(dev, qmp);
-
-	qcom_qmp_phy_typec_switch_register(qmp);
 
 	if (usb_np == dev->of_node)
 		phy_provider = devm_of_phy_provider_register(dev, qmp_combo_phy_xlate);
