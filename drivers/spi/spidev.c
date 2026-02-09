178ad611b0c979ec65227cbf0a67718557c7b9b1
Robin Reckmann <robin.reckmann@gmail.com>
2026-02-09 18:36:05 +0900

    Enable spi and panda spidev

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -705,6 +705,7 @@ static const struct class spidev_class = {
 static const struct spi_device_id spidev_spi_ids[] = {
 	{ .name = /* abb */ "spi-sensor" },
 	{ .name = /* cisco */ "spi-petra" },
+	{ .name = /* commaai */ "panda" },
 	{ .name = /* dh */ "dhcom-board" },
 	{ .name = /* elgin */ "jg10309-01" },
 	{ .name = /* gocontroll */ "moduline-module-slot"},
@@ -738,6 +739,7 @@ static int spidev_of_check(struct device *dev)
 static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "abb,spi-sensor", .data = &spidev_of_check },
 	{ .compatible = "cisco,spi-petra", .data = &spidev_of_check },
+	{ .compatible = "commaai,panda", .data = &spidev_of_check },
 	{ .compatible = "dh,dhcom-board", .data = &spidev_of_check },
 	{ .compatible = "elgin,jg10309-01", .data = &spidev_of_check },
 	{ .compatible = "gocontroll,moduline-module-slot", .data = &spidev_of_check},
